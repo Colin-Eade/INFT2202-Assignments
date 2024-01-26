68dde9c063d4c18f42d4b06dd277edf182e7912a
Megan Clarke <megan.clarke1@dcmail.ca>
2024-01-26 18:12:09 -0500

    team.html page mostly complete - need Colin bio

---
+++ Assignment1/scripts/project.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const projects = [
+    {
+        title: "Accessible Playground",
+        description: "We are thrilled to introduce a brand-new accessible playground, " +
+            "designed to ensure every child, regardless of their abilities, " +
+            "can enjoy the magic of playtime together.",
+        imageSrc: "img/portfolio/accessiblePlayground.png",
+    },
+    {
+        title: "Community Splash Pad",
+        description: "Get ready for refreshing fun as we embark on an exciting journey to " +
+            "bring a vibrant splashpad project to our community, where families can beat the " +
+            "heat, create lasting memories, and cool off in style.",
+        imageSrc: "img/portfolio/splashpad.png",
+    },
+    {
+        title: "Community Garden",
+        description: "Discover the beauty of nature and the power of community " +
+            "in our recently established community garden, a green oasis that not " +
+            "only enriches our environment but also provides a space for shared " +
+            "gardening experiences and a deeper connection to the earth.",
+        imageSrc: "img/portfolio/communityGarden.png",
+    },
+    {
+        title: "STEM Maker Space",
+        description: "Step into the future of learning with our state-of-the-art STEM Maker Space, " +
+            "where innovation knows no bounds. Discover a world of creativity, hands-on exploration, " +
+            "and limitless possibilities for students and community members alike.",
+        imageSrc: "img/portfolio/makerSpace.png",
+    },
+    {
+        title: "Community Recycling Program",
+        description: "We are thrilled to introduce our innovative Community Recycling Program, " +
+            "dedicated to making a greener, more sustainable future a reality in our community. " +
+            "Join us in reducing waste, conserving resources, and preserving our environment for " +
+            "generations to come.",
+        imageSrc: "project2.jpg",
+    },
+    {
+        title: "Senior Citizens' Social Center",
+        description: "Excitement fills the air as we prepare to unveil our newly completed Senior " +
+            "Citizens' Social Center. This milestone marks our commitment to ensuring that our senior " +
+            "residents have a vibrant space to call their own, where they can enjoy a multitude of " +
+            "activities, connect with peers, and bask in the warmth of a close-knit community.",
+        imageSrc: "project2.jpg",
+    },
+];
+
