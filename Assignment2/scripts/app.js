c07c39be015ae254152e75bda674d544581881ec
Megan Clarke <megan.clarke1@dcmail.ca>
2024-02-23 16:01:40 -0500

    add events.html page and functionality, update events.json file format for search

--- Assignment2/scripts/app.js
+++ Assignment2/scripts/app.js
@@ -11,56 +11,9 @@
 
 //region Globals
 const titlePrefix = "Harmony Hub - "
-const projects = [
-    {
-        title: "Accessible Playground",
-        description: "We are thrilled to introduce a brand-new accessible playground, " +
-            "designed to ensure every child, regardless of their abilities, " +
-            "can enjoy the magic of playtime together.",
-        imageSrc: "img/portfolio/accessiblePlayground.png",
-    },
-    {
-        title: "Community Splash Pad",
-        description: "Get ready for refreshing fun as we embark on an exciting journey to " +
-            "bring a vibrant splashpad project to our community, where families can beat the " +
-            "heat, create lasting memories, and cool off in style.",
-        imageSrc: "img/portfolio/splashpad.png",
-    },
-    {
-        title: "Community Garden",
-        description: "Discover the beauty of nature and the power of community " +
-            "in our recently established community garden, a green oasis that not " +
-            "only enriches our environment but also provides a space for shared " +
-            "gardening experiences and a deeper connection to the earth.",
-        imageSrc: "img/portfolio/communityGarden.png",
-    },
-    {
-        title: "STEM Maker Space",
-        description: "Step into the future of learning with our state-of-the-art STEM Maker Space, " +
-            "where innovation knows no bounds. Discover a world of creativity, hands-on exploration, " +
-            "and limitless possibilities for students and community members alike.",
-        imageSrc: "img/portfolio/makerSpace.png",
-    },
-    {
-        title: "Community Recycling Program",
-        description: "We are thrilled to introduce our innovative Community Recycling Program, " +
-            "dedicated to making a greener, more sustainable future a reality in our community. " +
-            "Join us in reducing waste, conserving resources, and preserving our environment for " +
-            "generations to come.",
-        imageSrc: "img/portfolio/recycling.png",
-    },
-    {
-        title: "Senior Citizens' Social Center",
-        description: "Excitement fills the air as we prepare to unveil our newly completed Senior " +
-            "Citizens' Social Center. This milestone marks our commitment to ensuring that our senior " +
-            "residents have a vibrant space to call their own, where they can enjoy a multitude of " +
-            "activities, connect with peers, and bask in the warmth of a close-knit community.",
-        imageSrc: "img/portfolio/seniorsCentre.png",
-    },
-];
 // end region
 
-// Format Functions
+//region Format Functions
 function formatDate(dateString) {
     const dateParts = dateString.split('-'); // Split the date string into parts
     const year = dateParts[0];
@@ -486,98 +439,60 @@ function formatDate(dateString) {
     /**
         Called to display the portfolio page and dynamically create the project cards for display
      */
-    function DisplayPortfolioPage(){
+    function DisplayPortfolioPage() {
         console.log("Called DisplayPortfolioPage...");
 
         const projectContainer = document.getElementById("project-container");
         const loadMoreButton = document.getElementById("load-more-button");
 
-        /**
-            Function to create a project card
-         */
-        function createProjectCard(project) {
-
-            const col = document.createElement("div");
-            col.classList.add("col", "d-flex", "justify-content-center");
-
-            const card = document.createElement("div");
-            card.classList.add("card");
-            // Set a maximum width for the card
-            card.style.maxWidth = "500px";
-
-            const image = document.createElement("img");
-            image.classList.add("card-img-top");
-            image.src = project.imageSrc;
-            image.alt = project.title;
-
-            // Set maximum width and height for the image
-            image.style.maxWidth = "500px"; // Adjust the value as needed
-            image.style.maxHeight = "200px"; // Adjust the value as needed
-
-            const cardBody = document.createElement("div");
-            cardBody.classList.add("card-body");
-
-            const title = document.createElement("h5");
-            title.classList.add("card-title");
-            title.textContent = project.title;
-
-            const description = document.createElement("p");
-            description.classList.add("card-text");
-
-            // Split the description by line breaks and create <br> elements
-            const descriptionLines = project.description.split('\n');
-            descriptionLines.forEach(line => {
-                const lineBreak = document.createElement("br");
-                description.appendChild(lineBreak);
-                description.appendChild(document.createTextNode(line));
-            });
-
-            cardBody.appendChild(title);
-            cardBody.appendChild(description);
-
-            card.appendChild(image);
-            card.appendChild(cardBody);
-
-            col.appendChild(card);
-
-            return col;
-        }
-
-        /**
-         * Function to display projects on the page
-         */
-        function displayProjects(startIndex, endIndex) {
-            for (let i = startIndex; i < endIndex && i < projects.length; i++) {
-                if (!projects[i].displayed) {
-                    const projectCard = createProjectCard(projects[i]);
-                    projectContainer.appendChild(projectCard);
-                    projects[i].displayed = true; // Mark the project as displayed
-                }
-            }
-        }
-
-        // Initialize the "displayed" property for all projects to false
-        for (let i = 0; i < projects.length; i++) {
-            projects[i].displayed = false;
-        }
-
-        let startIndex = 0;
-        const projectsPerPage = 2;
+        let projectsToShow = 2; // Number of initially displayed projects
 
         // Initial display of projects
-        displayProjects(startIndex, startIndex + projectsPerPage);
-        startIndex += projectsPerPage;
+        fetchProjectsData(displayProjects);
 
         // Event listener for the "Load More" button
         loadMoreButton.addEventListener("click", () => {
-            if (startIndex < projects.length) { // Check if there are more projects to display
-                displayProjects(startIndex, startIndex + projectsPerPage);
-                startIndex += projectsPerPage;
-            } else {
-                loadMoreButton.disabled = true; // Disable the button if there are no more projects
+            projectsToShow += 2; // Increment the number of projects to display
+            displayProjects(projectsToShow); // Display the updated list of projects
+        });
+    }
+
+    function fetchProjectsData(callback) {
+        $.ajax({
+            url: "./data/projects.json",
+            type: "GET",
+            dataType: "json",
+            success: function(data) {
+                callback(data.projects);
+            },
+            error: function(xhr, status, error) {
+                console.error('Error fetching projects data:', error);
             }
         });
     }
+
+    function displayProjects(projectsToShow) {
+        // Clear existing projects
+        projectContainer.innerHTML = "";
+
+        // Iterate through each project and create a card
+        for (let i = 0; i < projectsToShow && i < projects.length; i++) {
+            const projectCard = createProjectCard(projects[i]);
+            projectContainer.appendChild(projectCard);
+        }
+
+        // Show the "Show More" button if there are more projects to display
+        if (projectsToShow < projects.length) {
+            loadMoreButton.style.display = "block";
+        } else {
+            loadMoreButton.style.display = "none"; // Hide the button if all projects are displayed
+        }
+    }
+
+    // Call DisplayPortfolioPage after the document is loaded
+    document.addEventListener("DOMContentLoaded", function() {
+        DisplayPortfolioPage();
+    });
     //endregion
 
     //region Services Page Functions
@@ -760,6 +675,74 @@ function formatDate(dateString) {
      */
     function DisplayEventsPage() {
         console.log("Called DisplayEventsPage...");
+
+        $(document).ready(function() {
+            // Event listener for dropdown items
+            $('.dropdown-item').on('click', function() {
+                let filterOption = $(this).text().toLowerCase().trim();
+                displayEventCards(filterOption);
+            });
+
+            // Initial display of events with default filter option
+            displayEventCards('upcoming');
+        });
+
+    }
+
+    function displayEventCards(filterOption) {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', './data/events.json', true);
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                let eventsData = JSON.parse(xhr.responseText).events;
+
+                // Filter events based on the selected option
+                let filteredEvents = filterEvents(eventsData, filterOption);
+
+                // Sort filtered events by event date
+                filteredEvents.sort((a, b) => new Date(a.eventDate) - new Date(b.eventDate));
+
+                const eventsContainer = $("#events-container");
+                eventsContainer.empty(); // Clear existing events before adding filtered events
+
+                // Create cards for filtered events
+                filteredEvents.forEach(event => {
+                    const eventCard = createEventCard(event);
+                    eventsContainer.append(eventCard);
+                });
+            } else {
+                console.error('Error fetching JSON data:', xhr.statusText);
+            }
+        };
+        xhr.onerror = function() {
+            console.error('Error fetching JSON data:', xhr.statusText);
+        };
+        xhr.send();
+    }
+
+    function createEventCard(event) {
+        const card = $("<div>").addClass("card").css("margin", "5px");
+        const cardBody = $("<div>").addClass("card-body");
+
+        const title = $("<h5>").addClass("card-title").text(event.eventName);
+        const date = $("<p>").addClass("card-text").text("Date: " + formatDate(event.eventDate));
+        const location = $("<p>").addClass("card-text").text("Location: " + event.eventLocation);
+        const description = $("<p>").addClass("card-text").text(event.eventDescription);
+
+        cardBody.append(title, date, location, description);
+        card.append(cardBody);
+
+        return card;
+    }
+
+    function filterEvents(events, filterOption) {
+        if (filterOption === 'upcoming') {
+            return events.filter(event => new Date(event.eventDate) > new Date());
+        } else if (filterOption === 'past') {
+            return events.filter(event => new Date(event.eventDate) < new Date());
+        } else {
+            return events; // Return all events if filter option is 'all'
+        }
     }
     //endregion
 
