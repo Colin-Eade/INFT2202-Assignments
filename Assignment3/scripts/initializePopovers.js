e005b490a48e6be6755899cd2af229dfc730ccbf
Colin Eade <colin.eade@dcmail.ca>
2024-03-19 20:09:01 -0400

    Converted JavaScript to TypeScript

---
+++ Assignment3/scripts/initializePopovers.js
@@ -0,0 +1,6 @@
+"use strict";
+let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl);
+});
+//# sourceMappingURL=initializePopovers.js.map
