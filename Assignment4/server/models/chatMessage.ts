5eb295e28862a6c32c65b2a827f880b4487d9047
Colin Eade <colin.eade@dcmail.ca>
2024-04-13 17:31:41 -0400

    Added comments

--- Assignment4/server/models/chatMessage.ts
+++ Assignment4/server/models/chatMessage.ts
@@ -1,6 +1,5 @@
 import mongoose  from "mongoose";
 const Schema = mongoose.Schema;
-import passportLocalMongoose from "passport-local-mongoose";
 
 const ChatMessageSchema = new Schema({
     username: { type: String, required: true },
